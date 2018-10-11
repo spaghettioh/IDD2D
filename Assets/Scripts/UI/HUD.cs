2865bb1c83b57c3eb6d18a25f36821f4cf722255
Eric Mofield <ericmofield@gmail.com>
2018-10-11 00:47:26 -0400

    Setup basic HUD.

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HUD : MonoBehaviour {
+public class HUD : MonoBehaviour
+{
 
-    public FloatReference Health;
-    public string CurrentWeapon = "pistol";
+    public FloatVariable HealthVariable;
+    private string Health;
+    public Text CurrentHealth;
+
+    public FloatVariable ArmorVariable;
+    private string Armor;
+    public Text CurrentArmor;
+
+    public string Weapon = "pistol";
+    public Text CurrentWeapon;
 
     // Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start ()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        CurrentHealth.text = HealthVariable.Value.ToString();
+        CurrentArmor.text = ArmorVariable.Value.ToString();
+        CurrentWeapon.text = Weapon;
+    }
 }
